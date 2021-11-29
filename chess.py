15cf2b489c4c25bae699ace15636000153e99380
Ari <etin22@ncssm.edu>
2021-11-29 08:50:38 -0500

    Added in basic position and movement capacity. White = lower case.

--- chess.py
+++ chess.py
@@ -1,10 +1,13 @@
 print("I'm chessing!")
 def main():
-    print("we're doing stuff!")
-
-    print("Riley was here. This fetch/pull thing is interesting.")
-
-    gold="Au"
-    if gold != "Ag":
-        print("good")
+    position= [["R","N","B","Q","K","B","N","R"],["P","P","P","P","P","P","P","P"],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],["p","p","p","p","p","p","p","p"],["r","n","b","q","k","b","n","r"]]
+    print(move("21","41", position))
+def move (start, end, position):
+    start_row = int(start[0])-1
+    start_col = int(start[1])-1
+    end_row = int(end[0])-1
+    end_col = int(end[1])-1
+    position[end_row][end_col]=position[start_row][start_col]
+    position[start_row][start_col] = 0
+    return (position)
 main()
